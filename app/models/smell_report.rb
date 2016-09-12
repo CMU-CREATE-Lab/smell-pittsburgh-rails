8e1b8fc582da3f44bbff0b0ad1c82c0cd7556214
Yen-Chia Hsu <hsu.yenchia@gmail.com>
2016-09-12 12:24:13 -0400

    Added code to offset the lat/lng of smell reports for privacy.

--- app/models/smell_report.rb
+++ app/models/smell_report.rb
@@ -10,4 +10,13 @@ class SmellReport < ActiveRecord::Base
   validates :user_hash, :latitude, :longitude, :smell_value, :presence => true
   validates :smell_value, :inclusion => { in: (1..5) }
 
+  def self.perturbLatLng(lat, lng)
+    perturb_miles = 0.10
+    one_deg_of_lat_to_miles = 69
+    one_deg_of_lng_to_miles = Math.cos(lat*Math::PI/180)*69
+    lat_offset = (rand()*2 - 1)*perturb_miles / one_deg_of_lat_to_miles
+    lng_offset = (rand()*2 - 1)*perturb_miles / one_deg_of_lng_to_miles
+    return {"lat" => lat.to_f + lat_offset.to_f, "lng" => lng.to_f + lng_offset.to_f}
+  end
+
 end
