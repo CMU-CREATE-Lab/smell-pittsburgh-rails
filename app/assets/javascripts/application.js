67632a474f82e20a4a0eb260965d68a7ffaf11c7
Yen-Chia Hsu <legenddolphin@gmail.com>
2017-02-13 19:49:29 -0500

    Pulled the timeline chart visualization out to a library. Refactorted visualization.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 //= require util
 //= require googleAnalytics
 //= require visualization
+//= require FlatBlockChart
 
 //var user_hash = <%= params["user_hash"] %>;
--- app/assets/javascripts/visualization.js
+++ app/assets/javascripts/visualization.js
@@ -22,7 +22,8 @@ var $calendar;
 var init_date;
 
 // Map parameters
-var area, init_latlng;
+var area;
+var init_latlng;
 var init_zoom_desktop = 12;
 var init_zoom_mobile = 11;
 
@@ -31,17 +32,12 @@ var zoom_level_to_smell_icon_size = [24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24,
 var previous_icon_size;
 
 // Timeline variables
-var $timeline_index;
-var $timeline_date;
+var timeline;
 var $dialog_ok_button;
-var $timeline_container;
 var esdr_root_url = "https://esdr.cmucreatelab.org/api/v1/";
 var aqi_root_url = "http://api.smellpittsburgh.org/api/v1/get_aqi?city=";
-var staging_base_url = "http://api.smellpittsburgh.org";
 var api_url = "/api/v1/smell_reports?";
 var no_data_txt = "No data in last four hours.";
-var timelineTouched = false;
-var timelineTouchedPosition = {};
 
 var requests = [];
 
@@ -117,31 +113,11 @@ var sensorLoadCount = 0;
 var sensors = {};
 var totalSensors = sensorList.length;
 
-
-// tanh is not defined before Chrome 38
-// which means that Android <= 4.4.x will
-// throw an error when we attempt to use it;
-// so here is a pollyfill.
-var tanh_func = function (x) {
-  if (x === Infinity) {
-    return 1;
-  } else if (x === -Infinity) {
-    return -1;
-  } else {
-    var y = Math.exp(2 * x);
-    return (y - 1) / (y + 1);
-  }
-};
-Math.tanh = Math.tanh || tanh_func;
-
 function init() {
   // Store objects
-  $timeline_index = $("#timeline-index");
-  $timeline_date = $("#timeline-date");
   $calendar = $("#calendar");
   $calendar_dialog = $("#calendar-dialog");
   $dialog_ok_button = $("#dialog-ok-button");
-  $timeline_container = $("#timeline-container");
 
   // Create the page
   createGoogleMap();
@@ -156,7 +132,7 @@ function init() {
   });
 
   // Disable all href tags to prevent accidental link clicking on the map
-  $('body').on("click", "a", function(e) {
+  $('body').on("click", "a", function (e) {
     e.preventDefault();
   });
 
@@ -191,18 +167,18 @@ function createGoogleMap() {
   //default to Pittsburgh
   area = "PGH";
   init_latlng = {"lat": 40.42, "lng": -79.94};
-  init_date = new Date(2016,5,4);
+  init_date = new Date(2016, 5, 4);
   //get user location
   var query = window.location.search.slice(1).split("&");
-  for (var i=0;i<query.length;i++) {
+  for (var i = 0; i < query.length; i++) {
     var queryVar = query[i];
-    if(queryVar.indexOf("user_hash") != -1 && queryVar.match(/[A-Z]{2,}/)){
+    if (queryVar.indexOf("user_hash") != -1 && queryVar.match(/[A-Z]{2,}/)) {
       area = queryVar.match(/[A-Z]{2,}/)[0];
     }
   }
   if (area == "BA") {
-    init_latlng = {"lat":38.004472, "lng":-122.260693};
-    init_date = new Date(2017,0,1);
+    init_latlng = {"lat": 38.004472, "lng": -122.260693};
+    init_date = new Date(2017, 0, 1);
   }
 
   // Set Google map
@@ -215,7 +191,7 @@ function createGoogleMap() {
   });
 
   // Add events
-  map.addListener('zoom_changed', function() {
+  map.addListener('zoom_changed', function () {
     var zoom_level = map.getZoom();
     var icon_size = zoom_level_to_smell_icon_size[zoom_level];
     if (icon_size != previous_icon_size) {
@@ -259,18 +235,19 @@ function createCalendarDialog() {
     modal: true,
     width: 260,
     dialogClass: "custom-dialog noselect",
-    closeText:'<i class=\"fa fa-times\"></i>'
+    closeText: '<i class=\"fa fa-times\"></i>'
   });
   $calendar.on("change", function () {
     $calendar_dialog.dialog("close");
     var $selected = $calendar.find(":selected");
     if ($selected.val() == -1) {
-      selectMostRecentDate();
+      timeline.selectLastBlock();
     } else {
       var desired_index = smell_reports_jump_index[$selected.val()];
       if (typeof desired_index != "undefined") {
-        selectTimelineBtn($timeline_index.find("div[data-value=" + desired_index + "]"), true, false);
-        var data_time = (new Date($selected.data("year"), $selected.data("month")-1)).getTime();
+        timeline.selectBlockByIndex(desired_index);
+        // Google Analytics
+        var data_time = (new Date($selected.data("year"), $selected.data("month") - 1)).getTime();
         var label = {
           "dimension5": data_time.toString()
         };
@@ -303,7 +280,7 @@ function loadSmellReports(date) {
     success: function (data) {
       smell_reports = data;
       drawTimeline();
-      selectMostRecentDate();
+      timeline.selectLastBlock();
     },
     error: function (response) {
       console.log("server error:", response);
@@ -335,10 +312,6 @@ function genSmellURL(date_obj) {
   return root_url + api_url + api_paras;
 }
 
-function selectMostRecentDate() {
-  selectTimelineBtn($timeline_index.children().last().children().first(), true, false);
-}
-
 function drawSingleSmellReport(report_i) {
   var latlng = {"lat": report_i.latitude, "lng": report_i.longitude};
 
@@ -357,9 +330,9 @@ function drawSingleSmellReport(report_i) {
     created_date: date.getTime(),
     smell_value: report_i.smell_value,
     content: '<b>Date:</b> ' + date_str + '<br>'
-      + '<b>Smell Rating:</b> ' + smell_value + " (" + smell_value_text[smell_value - 1] + ")" + '<br>'
-      + '<b>Symptoms:</b> ' + feelings_symptoms + '<br>'
-      + '<b>Smell Description:</b> ' + smell_description,
+    + '<b>Smell Rating:</b> ' + smell_value + " (" + smell_value_text[smell_value - 1] + ")" + '<br>'
+    + '<b>Symptoms:</b> ' + feelings_symptoms + '<br>'
+    + '<b>Smell Description:</b> ' + smell_description,
     icon: {
       url: getSmellColor(report_i.smell_value - 1),
       scaledSize: new google.maps.Size(icon_size, icon_size),
@@ -428,16 +401,13 @@ function drawCalendar(data) {
   }
 }
 
-function deleteTimeline() {
-  $timeline_index.children().remove();
-  $timeline_date.children().remove();
-}
-
 function drawTimeline() {
-  var last_month, bounds;
+  var last_month;
+  var bounds;
   var td_count = 0;
   var date = init_date;
-  if(area == "BA") {
+  // Set the bound you filtering smell reports
+  if (area == "BA") {
     bounds = {
       max_lat: 38.8286208,
       min_lat: 36.906913,
@@ -445,7 +415,7 @@ function drawTimeline() {
       min_lng: -123.017998
     }
   }
-  else if(area == "PGH") {
+  else if (area == "PGH") {
     bounds = {
       max_lat: 40.916992,
       min_lat: 40.102992,
@@ -453,33 +423,29 @@ function drawTimeline() {
       min_lng: -80.471694
     }
   }
+  // Compute the average smell value
+  var data = [];
   for (var k = 0; k < smell_reports.length; k++) {
     var report_k = smell_reports[k];
-    //if (report_k.length == 0) {
-    //  continue;
-    //}
-    //var color = Math.round((0.95 - Math.tanh(report_k.length / 10)) * 255);
     var smell_average = 0;
     for (var i = 0; i < report_k.length; i++) {
       var report = report_k[i];
       if (report.latitude < bounds.max_lat && report.latitude > bounds.min_lat
-         && report.longitude < bounds.max_lng && report.longitude > bounds.min_lng) {
+        && report.longitude < bounds.max_lng && report.longitude > bounds.min_lng) {
         smell_average += report.smell_value;
       }
     }
     if (smell_average > 0)
       smell_average /= report_k.length;
-    var color = Math.round((0.95 - Math.tanh(smell_average / 5)) * 255);
-    var color_str = "rgb(" + color + "," + color + "," + color + ")";
     if (report_k[0])
       date = new Date(report_k[0].created_at);
     else
       date = new Date(date.setDate(date.getDate() + 1));
     var date_str = date.toDateString();
     var date_str_seg = date_str.split(" ");
-    // Add the day block and text
-    $timeline_index.append($('<td><div style="background-color: ' + color_str + '" class="custom-td-button" data-value="' + td_count + '" data-index="' + k + '" data-time="' + (new Date(date_str)).getTime()/1000 + '"></div></td>'));
-    $timeline_date.append($('<td>' + date_str_seg[1] + " " + date_str_seg[2] + '</td>'));
+    var label = date_str_seg[1] + " " + date_str_seg[2];
+    var epochtime = date.getTime() / 1000;
+    data.push([label, smell_average, epochtime]);
     // Save the index if necessary
     var month = date.getMonth();
     if (last_month != month) {
@@ -488,55 +454,34 @@ function drawTimeline() {
     }
     td_count++;
   }
-
-  // Add clicking events
-  $("#timeline-index .custom-td-button").on("click touchend", function (e) {
-    if (e.type == "click") timelineTouched = true;
-    if (timelineTouched)
-      selectTimelineBtn($(this), false, true);
-  });
-
-  $("#timeline-index .custom-td-button").on('touchstart', function(e) {
-    timelineTouchedPosition = {x: e.originalEvent.touches[0].pageX, y: e.originalEvent.touches[0].pageY};
-    timelineTouched = true;
-  });
-
-  $("#timeline-index .custom-td-button").on('touchmove', function(e) {
-    if (Math.abs(timelineTouchedPosition.x - e.originalEvent.touches[0].pageX) >= 2 || Math.abs(timelineTouchedPosition.y - e.originalEvent.touches[0].pageY) >= 2)
-      timelineTouched = false;
-  });
-}
-
-function selectTimelineBtn($ele, auto_scroll, from_click_event) {
-  if ($ele && !$ele.hasClass("selected-td-btn")) {
-    clearTimelineBtnSelection();
-    $ele.addClass("selected-td-btn");
-    infowindow_smell.close();
-    infowindow_sensor.close();
-    deleteAllSmellReports();
-    drawSmellReportsByIndex(parseInt($ele.data("index")));
-    deleteAllSensors();
-    var data_time = parseInt($ele.data("time"));
-    loadAndDrawAllSensors(data_time);
-    // Scroll to the position
-    if (auto_scroll) {
-      $timeline_container.scrollLeft(Math.round($ele.parent().position().left - $timeline_container.width() / 5));
-    }
-    if (from_click_event) {
+  // Use the charting library to draw the timeline
+  var chart_settings = {
+    click: function ($e) {
       // Add google analytics
       var label = {
-        "dimension5": (data_time*1000).toString()
+        "dimension5": (parseInt($e.data("epochtime")) * 1000).toString()
       };
       addGoogleAnalyticEvent("timeline", "click", label);
-    }
-  }
+    },
+    select: function ($e) {
+      selectTimelineButton(parseInt($e.data("index")), parseInt($e.data("epochtime")));
+    },
+    data: data,
+    format: ["label", "value", "epochtime"],
+    dataIndexForLabels: 0,
+    dataIndexForValues: 1
+
+  };
+  timeline = new EdaVizJS.FlatBlockChart("timeline-container", chart_settings);
 }
 
-function clearTimelineBtnSelection() {
-  var $ctb = $("#timeline-index .custom-td-button");
-  if ($ctb.hasClass("selected-td-btn")) {
-    $ctb.removeClass("selected-td-btn");
-  }
+function selectTimelineButton(index, epochtime) {
+  infowindow_smell.close();
+  infowindow_sensor.close();
+  deleteAllSmellReports();
+  drawSmellReportsByIndex(index);
+  deleteAllSensors();
+  loadAndDrawAllSensors(epochtime);
 }
 
 function isMobile() {
@@ -589,7 +534,7 @@ function loadAndDrawSingleSensor(time) {
   }
 
   // Load sensor data simultaneously
-  (function() {
+  (function () {
     // If we do not need to draw or already have lat/lng then immediately return a resolved Promise
     if (!sensor.doDraw || (sensors[sensor.name] && sensors[sensor.name].lat && sensors[sensor.name].lng))
       return $().promise();
@@ -601,7 +546,7 @@ function loadAndDrawSingleSensor(time) {
     });
     requests.push(xhr);
     return xhr;
-  })().then(function(){
+  })().then(function () {
     if (!use_PM25_now && !sensor.doDraw) return;
     var xhr = $.getJSON(data_url, function (response) {
       if (use_PM25_now) {
@@ -701,7 +646,7 @@ function drawSingleSensor(sensor) {
 
   // Add marker
   var image = new Image();
-  image.addEventListener("load", function() {
+  image.addEventListener("load", function () {
     var marker = new google.maps.Marker({
       position: latlng,
       map: map,
@@ -787,19 +732,19 @@ function deleteAllSensors() {
 
 function addTouchHorizontalScroll(elem) {
   var scrollStartPos, startTime, endTime, newPos, startTouchX, endTouchX;
-  $(elem).on("touchstart", function(e) {
+  $(elem).on("touchstart", function (e) {
     startTime = new Date().getTime();
     newPos = 0;
     endTouchX = null;
     startTouchX = e.originalEvent.touches[0].pageX;
     scrollStartPos = this.scrollLeft + startTouchX;
     e.preventDefault();
-  }).on("touchmove", function(e) {
+  }).on("touchmove", function (e) {
     endTouchX = e.originalEvent.touches[0].pageX;
     newPos = scrollStartPos - endTouchX;
     this.scrollLeft = newPos;
     e.preventDefault();
-  }).on("touchend touchcancel", function(e) {
+  }).on("touchend touchcancel", function (e) {
     // TODO: Flick/swip ability
     //endTime = new Date().getTime();
     //if (endTouchX && endTime - startTime < 100) {
@@ -843,6 +788,6 @@ function getSmellColor(idx) {
   }
 }
 
-$(function() {
+$(function () {
   init();
 });
