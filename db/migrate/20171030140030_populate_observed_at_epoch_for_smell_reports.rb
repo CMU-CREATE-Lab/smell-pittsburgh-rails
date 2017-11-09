d5e825124e57f5e0a676fa311fd96997a87ad6b6
mtasota <miketasota@gmail.com>
2017-11-09 16:42:48 +0000

    Fixed issues with migration (see docs for Model.reset_column_information)

--- db/migrate/20171030140030_populate_observed_at_epoch_for_smell_reports.rb
+++ db/migrate/20171030140030_populate_observed_at_epoch_for_smell_reports.rb
@@ -2,16 +2,22 @@ class PopulateObservedAtEpochForSmellReports < ActiveRecord::Migration
 
 
   def up
+    SmellReport.reset_column_information
     SmellReport.all.each do |report|
-      report.observed_at_epoch = report.observed_at.to_i
+      if report.observed_at.to_i > 0
+        report.observed_at_epoch = report.observed_at.to_i
+      else
+        report.observed_at_epoch = report.created_at.to_i
+      end
       report.save!
     end
   end
 
 
   def down
+    SmellReport.reset_column_information
     SmellReport.all.each do |report|
-      report.observed_at_epoch = report.observed_at.to_i
+      report.observed_at = Time.at(report.observed_at_epoch)
       report.save!
     end
   end
