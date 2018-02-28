d941fe8766cd36887bea009b7c9f312e2667f811
mtasota <miketasota@gmail.com>
2018-02-28 16:55:00 +0000

    Validate secret token for clients and generate random token on migration

--- db/migrate/20171019193315_populate_client_id_for_smell_reports.rb
+++ db/migrate/20171019193315_populate_client_id_for_smell_reports.rb
@@ -1,14 +1,19 @@
 class PopulateClientIdForSmellReports < ActiveRecord::Migration
   def up
+    time = Time.now.to_i.to_s
+    token1 = Digest::MD5.hexdigest time+rand.to_s
+    token2 = Digest::MD5.hexdigest time+rand.to_s
     smellpgh = Client.new
     smellpgh.id = CLIENT_ID_SMELLPGH
     smellpgh.name = "Smell Pittsburgh"
     smellpgh.description = "Cordova application targeted at iOS and Android platforms."
+    smellpgh.secret_token = token1
     smellpgh.save!
     bayarea = Client.new
     bayarea.id = CLIENT_ID_BA
     bayarea.name = "Air Watch: Bay Area"
     bayarea.description = "Fork of Smell PGH code base used in the Bay Area for reporting smells; makes use of 'additional_comments' field in SmellReport to add their own custom fields."
+    bayarea.secret_token = token2
     bayarea.save!
     SmellReport.all.each do |report|
       report.determine_client_for_deprecated_apis
--- db/migrate/20180227163627_add_secret_token_to_clients.rb
+++
@@ -1,5 +0,0 @@
-class AddSecretTokenToClients < ActiveRecord::Migration
-  def change
-    add_column :clients, :secret_token, :text
-  end
-end
