161e5d8ecca3ce0f4cdb553784a0b145e8419dd6
mtasota <miketasota@gmail.com>
2018-10-01 18:18:17 +0000

    Added v2 visualization link to routes

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
 
 
   get "/visualization" => "visualization#index"
+  get "/visualization/v2" => "visualization#index"
   get "/summary" => "summary#index"
 
 end
