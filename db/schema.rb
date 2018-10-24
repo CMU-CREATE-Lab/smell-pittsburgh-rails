ae42fd53342b458e9e2c388f472381ef20a0fe3b
Paul Dille <pdille@cmucreatelab.org>
2018-10-24 16:23:18 -0400

    - Added updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180803000003) do
+ActiveRecord::Schema.define(version: 20181001174852) do
 
   create_table "agencies", force: :cascade do |t|
     t.string   "name",       limit: 255
