27e5a08b0767e82c663909136321198a784eb30f
Paul Dille <pdille@cmucreatelab.org>
2018-11-14 14:50:16 -0500

    - Another fix to Jefferson markers migration

--- db/migrate/20180803000002_add_jeffersonky_markers.rb
+++ db/migrate/20180803000002_add_jeffersonky_markers.rb
@@ -2,17 +2,17 @@ class AddJeffersonkyMarkers < ActiveRecord::Migration
   def up
     markers = [
       [
-        {"name":"Firearms Training AirNow","sensors":[{"PM10":{"sources":[{"feed":4175,"channel":"PM10"}]}},{"PM2_5":{"sources":[{"feed":4175,"channel":"PM2_5"}]}},{"SO2":{"sources":[{"feed":4175,"channel":"SO2"}]}}]},
+        {"name":"Firearms Training AirNow","sensors":[{"PM10":{"sources":[{"feed":4175,"channel":"PM10"}]}},{"PM25":{"sources":[{"feed":4175,"channel":"PM2_5"}]}},{"SO2":{"sources":[{"feed":4175,"channel":"SO2"}]}}]},
       38.188805,
       -85.6767705,
       ],
       [
-        {"name":"WATSON AirNow","sensors":[{"PM10":{"sources":[{"feed":2921,"channel":"PM10"}]}},{"PM2_5":{"sources":[{"feed":2921,"channel":"PM2_5"}]}},{"PM2_5":{"sources":[{"feed":2921,"channel":"PM2_5_daily_max"}]}},{"PM2_5":{"sources":[{"feed":2921,"channel":"PM2_5_daily_mean"}]}},{"PM2_5":{"sources":[{"feed":2921,"channel":"PM2_5_daily_median"}]}},{"SO2":{"sources":[{"feed":2921,"channel":"SO2"}]}}]},
+        {"name":"WATSON AirNow","sensors":[{"PM10":{"sources":[{"feed":2921,"channel":"PM10"}]}},{"PM25":{"sources":[{"feed":2921,"channel":"PM2_5"}]}},{"PM25":{"sources":[{"feed":2921,"channel":"PM2_5_daily_max"}]}},{"PM25":{"sources":[{"feed":2921,"channel":"PM2_5_daily_mean"}]}},{"PM25":{"sources":[{"feed":2921,"channel":"PM2_5_daily_median"}]}},{"SO2":{"sources":[{"feed":2921,"channel":"SO2"}]}}]},
       38.188805,
       -85.6767705,
       ],
       [
-        {"name":"New Albany AirNow","sensors":[{"PM25":{"sources":[{"feed":2822,"channel":"PM2_5"}]}},{"PM2_5":{"sources":[{"feed":2822,"channel":"PM2_5_daily_max"}]}},{"PM2_5":{"sources":[{"feed":2822,"channel":"PM2_5_daily_mean"}]}},{"PM2_5":{"sources":[{"feed":2822,"channel":"PM2_5_daily_median"}]}},{"SO2":{"sources":[{"feed":2822,"channel":"SO2"}]}}]},
+        {"name":"New Albany AirNow","sensors":[{"PM25":{"sources":[{"feed":2822,"channel":"PM2_5"}]}},{"PM25":{"sources":[{"feed":2822,"channel":"PM2_5_daily_max"}]}},{"PM25":{"sources":[{"feed":2822,"channel":"PM2_5_daily_mean"}]}},{"PM25":{"sources":[{"feed":2822,"channel":"PM2_5_daily_median"}]}},{"SO2":{"sources":[{"feed":2822,"channel":"SO2"}]}}]},
       38.188805,
       -85.6767705,
       ],
@@ -22,7 +22,7 @@ class AddJeffersonkyMarkers < ActiveRecord::Migration
       -85.6767705,
       ],
       [
-        {"name":"Cannons Lane AirNow","sensors":[{"PM10":{"sources":[{"feed":2922,"channel":"PM10"}]}},{"PM2_5":{"sources":[{"feed":2922,"channel":"PM2_5"}]}},{"PM2_5":{"sources":[{"feed":2922,"channel":"PM2_5_daily_max"}]}},{"PM2_5":{"sources":[{"feed":2922,"channel":"PM2_5_daily_mean"}]}},{"PM2_5":{"sources":[{"feed":2922,"channel":"PM2_5_daily_median"}]}},{"SO2":{"sources":[{"feed":2922,"channel":"SO2"}]}}]},
+        {"name":"Cannons Lane AirNow","sensors":[{"PM10":{"sources":[{"feed":2922,"channel":"PM10"}]}},{"PM25":{"sources":[{"feed":2922,"channel":"PM2_5"}]}},{"PM25":{"sources":[{"feed":2922,"channel":"PM2_5_daily_max"}]}},{"PM25":{"sources":[{"feed":2922,"channel":"PM2_5_daily_mean"}]}},{"PM25":{"sources":[{"feed":2922,"channel":"PM2_5_daily_median"}]}},{"SO2":{"sources":[{"feed":2922,"channel":"SO2"}]}}]},
       38.188805,
       -85.6767705,
       ],
@@ -42,12 +42,12 @@ class AddJeffersonkyMarkers < ActiveRecord::Migration
       -85.6767705,
       ],
       [
-        {"name":"Southwick Community Center AirNow","sensors":[{"PM10":{"sources":[{"feed":2919,"channel":"PM10"}]}},{"PM2_5":{"sources":[{"feed":2919,"channel":"PM2_5"}]}},{"PM2_5":{"sources":[{"feed":2919,"channel":"PM2_5_daily_max"}]}},{"PM2_5":{"sources":[{"feed":2919,"channel":"PM2_5_daily_mean"}]}},{"PM2_5":{"sources":[{"feed":2919,"channel":"PM2_5_daily_median"}]}}]},
+        {"name":"Southwick Community Center AirNow","sensors":[{"PM10":{"sources":[{"feed":2919,"channel":"PM10"}]}},{"PM25":{"sources":[{"feed":2919,"channel":"PM2_5"}]}},{"PM25":{"sources":[{"feed":2919,"channel":"PM2_5_daily_max"}]}},{"PM25":{"sources":[{"feed":2919,"channel":"PM2_5_daily_mean"}]}},{"PM25":{"sources":[{"feed":2919,"channel":"PM2_5_daily_median"}]}}]},
       38.188805,
       -85.6767705,
       ],
       [
-        {"name":"BATES AirNow","sensors":[{"PM25":{"sources":[{"feed":2918,"channel":"PM2_5"}]}},{"PM2_5":{"sources":[{"feed":2918,"channel":"PM2_5_daily_max"}]}},{"PM2_5":{"sources":[{"feed":2918,"channel":"PM2_5_daily_mean"}]}},{"PM2_5":{"sources":[{"feed":2918,"channel":"PM2_5_daily_median"}]}}]},
+        {"name":"BATES AirNow","sensors":[{"PM25":{"sources":[{"feed":2918,"channel":"PM2_5"}]}},{"PM25":{"sources":[{"feed":2918,"channel":"PM2_5_daily_max"}]}},{"PM25":{"sources":[{"feed":2918,"channel":"PM2_5_daily_mean"}]}},{"PM25":{"sources":[{"feed":2918,"channel":"PM2_5_daily_median"}]}}]},
       38.188805,
       -85.6767705,
       ]
