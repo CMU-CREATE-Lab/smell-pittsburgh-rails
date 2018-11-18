5e49f6b68628ffe886dd458a6d87a87f982b9600
Paul Dille <pdille@cmucreatelab.org>
2018-11-18 02:07:35 -0500

    - Ensure if latLng is passed in, it is parsed and passed back to the view

--- app/controllers/visualization_controller.rb
+++ app/controllers/visualization_controller.rb
@@ -8,20 +8,22 @@ class VisualizationController < ApplicationController
     clientToken = params["client_token"].blank? ? "" : params["client_token"]
     client = Client.find_by_secret_token(clientToken) || Client.first
 
-    # Default to Pittsburgh
-    pgh = City.find(1)
-    @city = pgh
-    @latitude = pgh.latitude
-    @longitude = pgh.longitude
-    @zoom = pgh.zoom_level
+    if latLng.size == 2
+      # latLng is the GPS user location, passed from the app
+      @latitude = latLng[0]
+      @longitude = latLng[1]
+    else
+      # Default to Pittsburgh
+      pgh = City.find(1)
+      @city = pgh
+      @latitude = pgh.latitude
+      @longitude = pgh.longitude
+    end
 
     if zipCode
       zip_code = ZipCode.find_by_zip(zipCode)
       @city = zip_code.cities.first if zipCode
     elsif latLng.size == 2
-      # latLng is the GPS user location, passed from the app
-      @latitude = latLng[0]
-      @longitude = latLng[1]
       # request reverse geocode object
       geo = Geokit::Geocoders::GoogleGeocoder.reverse_geocode( "#{@latitude}, #{@longitude}" )
       zip_code = ZipCode.find_or_create_by(zip: geo.zip)
@@ -29,6 +31,7 @@ class VisualizationController < ApplicationController
     end
 
     @client_id = client.id
+    @zoom = @city.zoom_level
     @city = @city.to_json(:except => [:created_at, :updated_at, :app_metadata, :description]).html_safe
     @cities = City.all.to_json(:except => [:created_at, :updated_at, :app_metadata, :description]).html_safe
   end
