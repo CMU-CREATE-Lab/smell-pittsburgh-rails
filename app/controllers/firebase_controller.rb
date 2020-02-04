01960fd50ca984a847692cbb6e1d7f62ab84fa2c
Paul Dille <pdille@cmucreatelab.org>
2020-02-04 17:56:15 -0500

    - Expand notification testing in FirebaseController   * Before only SMC was supported

--- app/controllers/firebase_controller.rb
+++ app/controllers/firebase_controller.rb
@@ -7,15 +7,20 @@ class FirebaseController < ApplicationController
 
 
   def send_notification
-    # NOTE: assumes SMC
     topic = params["topic"]
     msg_title = params["title"]
     msg_body = params["body"]
+    app_type = params["app_type"]
 
     if topic.blank? or msg_title.blank? or msg_body.blank?
       response = { :error => "bad format" }
     else
-      FirebasePushNotification.send_smc_notification(topic, msg_title, msg_body)
+      # If app type is blank, asssume smc. This is here for legacy reasons
+      if app_type == "smc" || app_type == nil
+        FirebasePushNotification.send_smc_notification(topic, msg_title, msg_body)
+      elsif app_type == "smellpgh"
+        FirebasePushNotification.send_smellpgh_notification(topic, msg_title, msg_body)
+      end
       response = "OK"
     end
 
