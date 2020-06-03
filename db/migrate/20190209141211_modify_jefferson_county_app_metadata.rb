bc9ae0fa098825a1e3c083b0b379d7c5ea80e94d
Paul Dille <pdille@cmucreatelab.org>
2020-06-03 16:24:12 -0400

    - Modify location placeholder text for prior migrations   * Only useful for new database instances

--- db/migrate/20190209141211_modify_jefferson_county_app_metadata.rb
+++ db/migrate/20190209141211_modify_jefferson_county_app_metadata.rb
@@ -4,7 +4,7 @@ class ModifyJeffersonCountyAppMetadata < ActiveRecord::Migration
     city_obj.app_metadata = {
       "side_menu_background_url" => "/img/app-images/side-menu-backgrounds/louisville-ky.png",
       "side_menu_background_color" => "#24bab1",
-      "smell_description_placeholder_text" => "chemical, sewer-like, flowery, burnt",
+      "smell_description_placeholder_text" => "e.g. chemical, sewer-like, flowery, burnt",
       "additional_notes_placeholder_text" => "e.g. short-term vs. long-term pollution issue; multiple submissions in the same day"
     }
     city_obj.save!
