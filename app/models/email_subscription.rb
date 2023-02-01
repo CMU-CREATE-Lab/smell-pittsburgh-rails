15d010c8670f0a4aa7dc0ff106674817e0db1bd8
mtasota <miketasota@gmail.com>
2023-02-01 10:36:32 -0500

    include token to unsubscribe in email subscription model

--- app/models/email_subscription.rb
+++ app/models/email_subscription.rb
@@ -1,3 +1,19 @@
 class EmailSubscription < ActiveRecord::Base
   validates :email, :uniqueness => true
+  validates :token_to_unsubscribe, :uniqueness => true
+
+  before_save :generate_token_to_unsubscribe
+
+  # static variable avoids duplicate UIDs being created at same time
+  @@uid_iterator = 0
+
+  def self.uid_iterator
+    @@uid_iterator
+  end
+
+  private
+    def generate_token_to_unsubscribe
+      @@uid_iterator = (@@uid_iterator + 1) % 1000
+      self.token_to_unsubscribe = Digest::MD5.hexdigest("#{@@uid_iterator}-#{Time.now.to_i}")
+    end
 end
--- db/migrate/20230131214713_create_email_subscriptions.rb
+++ db/migrate/20230131214713_create_email_subscriptions.rb
@@ -2,6 +2,7 @@ class CreateEmailSubscriptions < ActiveRecord::Migration
   def change
     create_table :email_subscriptions do |t|
       t.string :email
+      t.string :token_to_unsubscribe
       t.boolean :subscribe_admin, :default => false
       t.boolean :subscribe_bcamp, :default => false
 
