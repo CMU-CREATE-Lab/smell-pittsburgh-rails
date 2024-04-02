5c0e7fe336f2373d2b3e54f927e2e8ccb9e58f5a
mtasota <miketasota@gmail.com>
2024-04-02 17:34:41 -0400

    add db/bcamp_data.sqlite3 to capistrano shared

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :deploy_to, "/var/www/rails-apps/#{fetch(:application)}/#{fetch(:stage)}"
 
 # shared resources
 set :linked_dirs, %w(log public/system tmp/cache)
-set :linked_files, %w(config/database.yml config/secrets.yml config/environment.rb config/environments/production.rb)
+set :linked_files, %w(config/database.yml config/secrets.yml config/environment.rb config/environments/production.rb db/bcamp_data.sqlite3)
 
 # git/version control info
 set :scm, :git
